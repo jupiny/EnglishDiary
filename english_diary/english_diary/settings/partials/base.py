af927cb8ca677d0de2c83ab078cbf7586e90246e
jupiny <tmdghks584@gmail.com>
2016-06-26 21:45:43 +0900

    install django_bootstrap_calendar: exclude django_bootstrap_calendar from .pep settings

--- english_diary/english_diary/settings/partials/base.py
+++ english_diary/english_diary/settings/partials/base.py
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_bootstrap_calendar',
 ]
 
 MIDDLEWARE_CLASSES = [
