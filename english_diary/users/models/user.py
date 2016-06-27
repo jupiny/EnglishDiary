04f6a9cbe487dab9a27ea7983e49d8ea5090000a
samkugji <samkugji@gmail.com>
2016-06-27 19:50:31 +0900

    update user.py: add hash_id / fix for pep8

--- english_diary/users/models/user.py
+++ english_diary/users/models/user.py
@@ -2,6 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-
 class User(AbstractUser):
-    pass
+
+    hash_id = models.CharField(
+        max_length=8,
+        blank=True,
+        null=True,
+    )
+
+    # TODO: ImageField with pillow
+
+    # TODO: created_at, updated_at
