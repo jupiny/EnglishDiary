83358ebacf732fe5a53a791d2c5bfb8120a93525
samkugji <samkugji@gmail.com>
2016-06-27 19:13:35 +0900

    init users-models Abstract User setting without anything.

--- english_diary/english_diary/settings/partials/base.py
+++ english_diary/english_diary/settings/partials/base.py
@@ -1,5 +1,6 @@
 import os
 
+AUTH_USER_MODEL = 'users.User'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
