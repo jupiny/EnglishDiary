1c35a0d9e5e428818575f15690f7b151cfb67af4
jupiny <tmdghks584@gmail.com>
2016-06-29 22:34:52 +0900

    init Diary model

---
+++ english_diary/diaries/models/diary.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+from users.models import User
+
+
+class Diary(models.Model):
+
+    user = models.ForeignKey(User)
+    content = models.TextField()
+
+    hash_id = models.CharField(
+        max_length=8,
+        blank=True,
+        null=True,
+    )
+
+    diary_image = models.ImageField(
+        blank=True,
+        null=True,
+    )
+
+    word_count = models.PositiveIntegerField(
+        blank=True,
+        null=True,
+        default=0,
+    )
+
+    diary_type = models.PositiveSmallIntegerField(
+        blank=True,
+        null=True,
+        default=0,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
