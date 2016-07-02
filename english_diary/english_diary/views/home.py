2433b1b0aaf9f855b3e55bfffdf67f4dae874092
jupiny <tmdghks584@gmail.com>
2016-07-02 23:28:46 +0900

    update HomeView: set today on calendar

--- english_diary/english_diary/views/home.py
+++ english_diary/english_diary/views/home.py
@@ -1,12 +1,16 @@
 from django.views.generic import View
 from django.shortcuts import render
+import datetime
 
 
 class HomeView(View):
 
     def get(self, request, *args, **kwargs):
+        today = datetime.datetime.now().strftime("%Y-%m-%d")
         return render(
             request,
             "home.html",
-            context={},
+            context={
+                "today": today,     
+            },
         )
