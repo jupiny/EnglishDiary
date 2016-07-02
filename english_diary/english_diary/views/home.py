3c8ff360ebc89b6206d044e4de526e44b4e4a44c
jupiny <tmdghks584@gmail.com>
2016-07-02 23:36:43 +0900

    refactor HomeView: from datetime import datetime

--- english_diary/english_diary/views/home.py
+++ english_diary/english_diary/views/home.py
@@ -1,12 +1,12 @@
 from django.views.generic import View
 from django.shortcuts import render
-import datetime
+from datetime import datetime
 
 
 class HomeView(View):
 
     def get(self, request, *args, **kwargs):
-        today = datetime.datetime.now().strftime("%Y-%m-%d")
+        today = datetime.now().strftime("%Y-%m-%d")
         return render(
             request,
             "home.html",
