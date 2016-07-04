bb42ac17f24fdcba9e6771fa40b6f5f6dbb15596
jupiny <tmdghks584@gmail.com>
2016-07-05 02:15:40 +0900

    fix DiaryDetailAPIView: inherit APIView to catch DoesNotExist Exception

--- english_diary/diaries/api/detail.py
+++ english_diary/diaries/api/detail.py
@@ -1,13 +1,28 @@
-from diaries.serializers import DiaryModelSerializer
-
-from rest_framework.generics import RetrieveAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response 
+from rest_framework import status 
 
 from diaries.models import Diary
 
 
-class DiaryDetailAPIView(RetrieveAPIView):
-    
-    queryset = Diary.objects.all()
-    serializer_class = DiaryModelSerializer
-    lookup_field = "datetime"
-    lookup_url_kwarg = "datetime"
+class DiaryDetailAPIView(APIView):
+
+    def get(self, request, *args, **kwargs):
+
+        try:
+            diary = Diary.objects.get(datetime=kwargs.get("datetime"))
+
+            # Diary is exist
+            content = diary.content
+
+        except Diary.DoesNotExist:
+
+            #Diary is not exist
+            content = ""
+
+        return Response(
+            status=status.HTTP_201_CREATED,
+            data={
+                "content": content,
+            },
+        )
--- english_diary/diaries/serializers/__init__.py
+++
@@ -1,1 +0,0 @@
-from .diary import DiaryModelSerializer
