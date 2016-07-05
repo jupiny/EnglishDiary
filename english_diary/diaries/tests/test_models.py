2bbabcb952627a13a7ace1e143e54eca22ddec8e
jupiny <tmdghks584@gmail.com>
2016-07-05 18:50:46 +0900

    uddate DiaryModelTestCase: add datetime when create a diary

--- english_diary/diaries/tests/test_models.py
+++ english_diary/diaries/tests/test_models.py
@@ -18,6 +18,7 @@ class DiaryModelTestCase(TestCase):
         # Create a diary
         self.diary = self.user.diary_set.create(
             content="Today I am very happy!",
+            datetime="2016/07/01",
         )
 
     def test_diary_model_should_have_correct_word_count(self):
