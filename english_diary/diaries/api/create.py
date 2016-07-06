a8f2f4cda709886957e77ae78df610e14e430b40
jupiny <tmdghks584@gmail.com>
2016-07-06 15:08:30 +0900

    update DiaryCreateAPIView: search Korean in content

--- english_diary/diaries/api/create.py
+++ english_diary/diaries/api/create.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+import re
 
 from diaries.models import Diary
 
@@ -12,21 +13,32 @@ class DiaryCreateAPIView(APIView):
         content = request.POST.get("content")
         diary = Diary.objects.get_or_none(datetime=datetime)
 
-        if diary:
-            # Update Diary
-            diary.datetime = datetime
-            diary.content = content
-            diary.save()
+        # Search Korean in text
+        has_korean = re.search(
+            r".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*",
+            content,
+        )
+
+        if not has_korean:
+            if diary:
+                # Update Diary
+                diary.datetime = datetime
+                diary.content = content
+                diary.save()
+            else:
+                # Create Diary
+                Diary.objects.create(
+                    user=request.user,
+                    datetime=datetime,
+                    content=content,
+                )
+            result = True
         else:
-            # Create Diary
-            Diary.objects.create(
-                user=request.user,
-                datetime=datetime,
-                content=content,
-            )
+            result = False
 
         return Response(
             status=status.HTTP_201_CREATED,
             data={
+                "result": result,
             },
         )
