d4fb1034e221c558eb543bfa65d2b0a01de34152
samkugji <samkugji@gmail.com>
2016-07-12 15:50:03 +0900

    fix urls/api.py: add comma

--- english_diary/english_diary/urls/api.py
+++ english_diary/english_diary/urls/api.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'^diary/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/delete/$',
         DiaryDeleteAPIView.as_view(), name="delete"),
     url(r'^dict/(?P<find_word>\w+)/$', NaverDictionaryAPIView.as_view(), name="dict"),
-    url(r'translate/$', TranslateAPIView.as_view(), name='translate')
+    url(r'translate/$', TranslateAPIView.as_view(), name='translate'),
     url(r'^translate/$', TranslateAPIView.as_view(), name='translate'),
 ]
