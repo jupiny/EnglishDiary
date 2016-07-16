d056e5bbc56ac9b43bb012622070391a92f69ccb
samkugji <samkugji@gmail.com>
2016-07-16 21:47:47 +0900

    fix analysis_basic.js: add not selector for excluding ".old"

--- english_diary/english_diary/static/js/analysis_basic.js
+++ english_diary/english_diary/static/js/analysis_basic.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#analysis_basic').click(function() {
 
-        var diaryWrittenElement = $(".day.diary-written").not(".new");
+        var diaryWrittenElement = $(".day.diary-written").not(".new").not(".old");
         var countDiaryWrittenElement = diaryWrittenElement.length; 
         var message = "한 달중 쓴 총 일기 : "+countDiaryWrittenElement;
         alert(message);
