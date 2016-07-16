1dea76a265167caae8615fd7a56aeb70a3a4f217
jupiny <tmdghks584@gmail.com>
2016-07-16 21:49:37 +0900

    init send_email utils: refactor SendEmailTask

--- english_diary/users/tasks/send_email_task.py
+++ english_diary/users/tasks/send_email_task.py
@@ -1,22 +1,18 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
 
-import requests
 from celery import Task
+
+from users.utils.send_email import send_email
 
 
 class SendEmailTask(Task):
 
     def run(self, user_id):
         user = get_user_model().objects.get(pk=user_id)
-        requests.post(
-            settings.MAILGUN_API_MESSAGE_URL,
-            auth=("api", settings.MAILGUN_API_KEY),
-            data={
-                "from": settings.MAILGUN_SENDER_EMAIL,
-                "to": [
-                    user.email,
-                ],
-                "subject": settings.MAILGUN_SUBJECT,
-                "text": user.username+settings.MAILGUN_TEXT,
-            })
+        send_email(
+            sender=settings.MAILGUN_SENDER_EMAIL,
+            receiver=user.email,
+            subject=settings.MAILGUN_SUBJECT,
+            text=user.username+settings.MAILGUN_TEXT,
+        )
