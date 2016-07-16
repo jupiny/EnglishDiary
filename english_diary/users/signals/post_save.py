78b4833a9198ee3f966bfff78dcd1d744274e5b9
jupiny <tmdghks584@gmail.com>
2016-07-16 18:30:02 +0900

    init SendEmailTask: refactor post_save_user using celery

--- english_diary/users/signals/post_save.py
+++ english_diary/users/signals/post_save.py
@@ -1,22 +1,12 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-from django.conf import settings
 
-import requests
+from users.tasks import SendEmailTask
 
 
 @receiver(post_save, sender=get_user_model())
 def post_save_user(sender, instance, created, **kwargs):
     if created:
-        requests.post(
-            settings.MAILGUN_API_MESSAGE_URL,
-            auth=("api", settings.MAILGUN_API_KEY),
-            data={
-                "from": settings.MAILGUN_SENDER_EMAIL,
-                "to": [
-                    instance.email,
-                ],
-                "subject": settings.MAILGUN_SUBJECT,
-                "text": instance.username+settings.MAILGUN_TEXT,
-            })
+        task = SendEmailTask()
+        task.delay(instance.id)
