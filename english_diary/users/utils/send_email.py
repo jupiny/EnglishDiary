9bd439ab685c036ff347d2d5ea8cc4f593867b0a
jupiny <tmdghks584@gmail.com>
2016-07-16 21:49:37 +0900

    init send_email utils: refactor SendEmailTask

---
+++ english_diary/users/utils/send_email.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+
+import requests
+
+
+def send_email(*args, **kwargs):
+    sender = kwargs.get("sender")
+    receiver = kwargs.get("receiver")
+    subject = kwargs.get("subject")
+    text = kwargs.get("text")
+    requests.post(
+        settings.MAILGUN_API_MESSAGE_URL,
+        auth=("api", settings.MAILGUN_API_KEY),
+        data={
+            "from": sender,
+            "to": [
+                receiver,
+            ],
+            "subject": subject,
+            "text": text,
+        })
