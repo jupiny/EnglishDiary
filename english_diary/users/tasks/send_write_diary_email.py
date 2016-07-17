29366d6b23caefa390abb0c2f401844ea1827d06
jupiny <tmdghks584@gmail.com>
2016-07-17 19:40:01 +0900

    update SendWriteDiaryEmailTask: send email to users who didn't write diary

--- english_diary/users/tasks/send_write_diary_email.py
+++ english_diary/users/tasks/send_write_diary_email.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from datetime import datetime
 
 from celery import Task
 
@@ -9,14 +10,16 @@ from users.utils.send_email import send_email
 class SendWriteDiaryEmailTask(Task):
 
     def run(self):
+        today = datetime.now().strftime("%Y/%m/%d")
         for user in get_user_model().objects.all():
-            send_email(
-                sender=settings.ADMIN_SENDER_EMAIL,
-                receiver=user.email,
-                subject=settings.WRITE_DIARY_EMAIL_SUBJECT.format(
-                    username=user.username,
-                ),
-                text=settings.WRITE_DIARY_EMAIL_TEXT.format(
-                    username=user.username,
+            if not user.diary_set.get_or_none(datetime=today):
+                send_email(
+                    sender=settings.ADMIN_SENDER_EMAIL,
+                    receiver=user.email,
+                    subject=settings.WRITE_DIARY_EMAIL_SUBJECT.format(
+                        username=user.username,
+                    ),
+                    text=settings.WRITE_DIARY_EMAIL_TEXT.format(
+                        username=user.username,
+                    )
                 )
-            )
