e0e02ffd30aa20796a79c46de17202d0b97e35a7
jupiny <tmdghks584@gmail.com>
2016-07-20 19:35:58 +0900

    update database settings: change database url to environment variable

--- english_diary/english_diary/settings/partials/database.py
+++ english_diary/english_diary/settings/partials/database.py
@@ -1,14 +1,24 @@
 import os
 
+import dj_database_url
+
 from .base import BASE_DIR
 
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# dj-database-url
+# https://github.com/kennethreitz/dj-database-url
+
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(
+    default=os.environ.get("DATABASE_URL"),
+)
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
