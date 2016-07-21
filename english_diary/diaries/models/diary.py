15d3da63a4c9d3c329a0b8d7ce031c05bc8db714
jupiny <tmdghks584@gmail.com>
2016-07-21 19:35:15 +0900

    update Diary, User models: remove unused fields

--- english_diary/diaries/models/diary.py
+++ english_diary/diaries/models/diary.py
@@ -21,29 +21,12 @@ class Diary(models.Model):
     user = models.ForeignKey(User)
     content = models.TextField()
 
-    hash_id = models.CharField(
-        max_length=8,
-        blank=True,
-        null=True,
-    )
-
-    diary_image = models.ImageField(
-        blank=True,
-        null=True,
-    )
-
     @property
     def used_words(self):
         diary_content = self.content.lower()
         for puctuation_mark in settings.PUCTUATION_MARKS:
             diary_content = diary_content.replace(puctuation_mark, "")
         return list(set(diary_content.split()))
-
-    diary_type = models.PositiveSmallIntegerField(
-        blank=True,
-        null=True,
-        default=0,
-    )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- english_diary/users/models/user.py
+++ english_diary/users/models/user.py
@@ -4,22 +4,4 @@ from django.db import models
 
 class User(AbstractUser):
 
-    hash_id = models.CharField(
-        max_length=8,
-        blank=True,
-        null=True,
-    )
-
-    profile_image = models.ImageField(
-        blank=True,
-        null=True,
-    )
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    # TODO : 어떤 필드로 해야 하나 + 자동저장은 post_save() 처럼 하세요
-    words = models.TextField(
-        blank=True,
-        null=True,
-    )
+    pass
