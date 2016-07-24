edc53ecce5845a874a8fa959310e315ae439971d
jupiny <tmdghks584@gmail.com>
2016-07-24 23:40:27 +0900

    update javascript file: add confirm function when delete user or diary

--- english_diary/english_diary/static/js/mypage.js
+++ english_diary/english_diary/static/js/mypage.js
@@ -118,7 +118,7 @@ $( document ).ready(function() {
                     console.log(error);
                 }
             });
-            return false;
         }
+        return false;
     });
 });
