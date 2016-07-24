bac426a7d37bbe376f28283c1c5be83e5028ce4e
jupiny <tmdghks584@gmail.com>
2016-07-24 15:56:11 +0900

    update UserEmailAPIView: delete unecessary import code

--- english_diary/users/api/user/email.py
+++ english_diary/users/api/user/email.py
@@ -1,7 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth import get_user_model
 
 
 class UserEmailAPIView(APIView):
