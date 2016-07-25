9d0a801df26271fbe0ff424439c54a54530f3315
jupiny <tmdghks584@gmail.com>
2016-07-25 18:21:19 +0900

    update send email tast: send email to only users who have email

--- english_diary/users/tasks/send_signup_email.py
+++ english_diary/users/tasks/send_signup_email.py
@@ -10,11 +10,12 @@ class SendSignupEmailTask(Task):
 
     def run(self, user_id):
         user = get_user_model().objects.get(pk=user_id)
-        send_email(
-            sender=settings.ADMIN_SENDER_EMAIL,
-            receiver=user.email,
-            subject=settings.SIGNUP_EMAIL_SUBJECT,
-            text=settings.SIGNUP_EMAIL_TEXT.format(
-                username=user.username,
+        if user.email:
+            send_email(
+                sender=settings.ADMIN_SENDER_EMAIL,
+                receiver=user.email,
+                subject=settings.SIGNUP_EMAIL_SUBJECT,
+                text=settings.SIGNUP_EMAIL_TEXT.format(
+                    username=user.username,
+                )
             )
-        )
