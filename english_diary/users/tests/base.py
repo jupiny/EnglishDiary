bf24ac2d1708c11b8d0091e2a694b5288c2af2b2
jupiny <tmdghks584@gmail.com>
2016-07-28 00:20:53 +0900

    update UserModelTestCase: add test code related to monthly_words, whole_used_words

--- english_diary/users/tests/base.py
+++ english_diary/users/tests/base.py
@@ -6,6 +6,8 @@ from rest_framework.test import APIClient
 
 from english_diary.celery import app
 from users.signals.post_save import post_save_user
+from diaries.signals.post_save import post_save_diary
+from diaries.models import Diary
 
 
 class UserBaseTestCase(TestCase):
@@ -14,6 +16,7 @@ class UserBaseTestCase(TestCase):
 
         # Disable Signals
         post_save.disconnect(post_save_user, sender=get_user_model())
+        post_save.disconnect(post_save_diary, sender=Diary)
 
         # Run celery task synchronous
         app.conf.update(CELERY_ALWAYS_EAGER=True)
@@ -34,4 +37,22 @@ class UserBaseTestCase(TestCase):
         self.client.login(
             username=self.test_username,
             password=self.test_password,
+        )
+
+        # Create diaries
+        self.user.diary_set.create(
+            datetime="2016/07/02",
+            content="Today it was very hot!",
+        )
+        self.user.diary_set.create(
+            datetime="2016/07/15",
+            content="I went to the concert. And I had dinner with my friends",
+        )
+        self.user.diary_set.create(
+            datetime="2016/08/11",
+            content="I watched a action movie with my family. It was so exciting.",
+        )
+        self.user.diary_set.create(
+            datetime="2016/08/29",
+            content="I was so tired today. I don`t want to do no more",
         )
