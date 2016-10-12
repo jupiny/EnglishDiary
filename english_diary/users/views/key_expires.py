2c778095ada7d78aac858b3f1a30b0cd1eace1a9
jupiny <tmdghks584@gmail.com>
2016-10-12 15:50:15 +0900

    update KeyExpiresView: redirect home if user verification_key doesn't expire

--- english_diary/users/views/key_expires.py
+++ english_diary/users/views/key_expires.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.urlresolvers import reverse
 from django.views.generic import View
+
+import datetime
+
+from users.models import Profile
 
 
 class KeyExpiresView(View):
 
     def get(self, request, *args, **kwargs):
+        verification_key = kwargs.get("verification_key")
+        profile = get_object_or_404(Profile, verification_key=verification_key)
+        user = profile.user
+
+        # Check if user is verified or user verification_key expires
+        if user.is_verified or datetime.datetime.now() <= profile.key_expires:
+            return redirect(reverse("home"))
+
         return render(
             request,
             "users/key_expires.html",
