bb19d600bf0347992a6d7a9a5c6eabcbd9f3a894
jupiny <tmdghks584@gmail.com>
2016-10-15 02:56:08 +0900

    rename ADMIN_SENDER_EMAIL: fix bug that occured when send email to daum domain

--- english_diary/english_diary/settings/partials/mailgun.py
+++ english_diary/english_diary/settings/partials/mailgun.py
@@ -5,7 +5,7 @@ MAILGUN_API_BASE_URL = os.environ.get("MAILGUN_API_BASE_URL")
 MAILGUN_API_MESSAGE_URL = MAILGUN_API_BASE_URL+"messages"
 MAILGUN_API_KEY = os.environ.get("MAILGUN_API_KEY")
 
-ADMIN_SENDER_EMAIL = "noreply@eng_diary_in_hard_way.com"
+ADMIN_SENDER_EMAIL = "noreply@engdiary.dsds101.info"
 
 EMAIL_VERIFICATION_SUBJECT = "[깐깐하게 쓰는 영어일기] {username}님, 회원가입을 환영합니다. 이메일 주소를 인증해주세요."
 
